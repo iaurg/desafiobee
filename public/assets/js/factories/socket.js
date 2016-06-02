316aea273ec989852e075a8da02a46eb1dab1146
Diego Schell Fernandes <diego.schell.f@gmail.com>
2016-06-02 08:29:33 -0300

    Socket factory url

--- public/assets/js/factories/socket.js
+++ public/assets/js/factories/socket.js
@@ -3,12 +3,6 @@
 
   angular.module('DesafioBee')
     .factory('socket', function (socketFactory) {
-      var myIoSocket = io.connect('http://localhost:8080');
-
-      var socket = socketFactory({
-          ioSocket: myIoSocket
-      });
-
-      return socket;
+      return socketFactory();
     });
 })();
